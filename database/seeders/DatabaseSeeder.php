ba79a2f6ffce8d91ec3b938c4fe1daed19af1a44
m4ndil <mandilshresthaktm@gmail.com>
2024-03-21 12:38:45 +0545

    Student => autogenerate reg_no into reg_no column, add id, fix image alt

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,22 +1,48 @@
 <?php
 
-namespace Database\Seeders;
+namespace Database\Seeds;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create(); oracle conn
+        $users = [
+            [
+                'id'             => 1,
+                'reg_no'         => 'NCCS0003',
+                'email'          => 'admin@admin.com',
+                'password'       => Hash::make('password'),
+                'remember_token' => null,
+                'created_at'     => '2023-12-17 14:13:32',
+                'updated_at'     => '2023-12-17 14:13:32',
+                'deleted_at'     => null,
+            ],
+            [
+                'id'             => 2,
+                'reg_no'         => 'NCCS0002',
+                'email'          => 'teacher@teacher.com',
+                'password'       => Hash::make('password'),
+                'remember_token' => null,
+                'created_at'     => '2023-12-17 14:13:32',
+                'updated_at'     => '2023-12-17 14:13:32',
+                'deleted_at'     => null,
+            ],
+            [
+                'id'             => 3,
+                'reg_no'         => 'NCCS0001',
+                'email'          => 'student@student.com',
+                'password'       => Hash::make('password'),
+                'remember_token' => null,
+                'created_at'     => '2023-12-17 14:13:32',
+                'updated_at'     => '2023-12-17 14:13:32',
+                'deleted_at'     => null,
+            ]
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::insert($users);
     }
 }
