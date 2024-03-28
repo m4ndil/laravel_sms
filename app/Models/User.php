fc27a9f553e8658be16fd096d5933df20b7338b5
m4ndil <mandilshresthaktm@gmail.com>
2024-03-28 13:56:50 +0545

    api stubs

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,3 +43,83 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 }
+
+class Das {
+  String? _leaveType;
+  String? _startDate;
+  String? _endDate;
+  String? _requestDate;
+  String? _reason;
+  String? _status;
+  String? _totalLeaveDays;
+
+  Das(
+      {String? leaveType,
+      String? startDate,
+      String? endDate,
+      String? requestDate,
+      String? reason,
+      String? status,
+      String? totalLeaveDays}) {
+    if (leaveType != null) {
+      this._leaveType = leaveType;
+    }
+    if (startDate != null) {
+      this._startDate = startDate;
+    }
+    if (endDate != null) {
+      this._endDate = endDate;
+    }
+    if (requestDate != null) {
+      this._requestDate = requestDate;
+    }
+    if (reason != null) {
+      this._reason = reason;
+    }
+    if (status != null) {
+      this._status = status;
+    }
+    if (totalLeaveDays != null) {
+      this._totalLeaveDays = totalLeaveDays;
+    }
+  }
+
+  String? get leaveType => _leaveType;
+  set leaveType(String? leaveType) => _leaveType = leaveType;
+  String? get startDate => _startDate;
+  set startDate(String? startDate) => _startDate = startDate;
+  String? get endDate => _endDate;
+  set endDate(String? endDate) => _endDate = endDate;
+  String? get requestDate => _requestDate;
+  set requestDate(String? requestDate) => _requestDate = requestDate;
+  String? get reason => _reason;
+  set reason(String? reason) => _reason = reason;
+  String? get status => _status;
+  set status(String? status) => _status = status;
+  String? get totalLeaveDays => _totalLeaveDays;
+  set totalLeaveDays(String? totalLeaveDays) =>
+      _totalLeaveDays = totalLeaveDays;
+
+  Das.fromJson(Map<String, dynamic> json) {
+    _leaveType = json['leaveType'];
+    _startDate = json['startDate'];
+    _endDate = json['endDate'];
+    _requestDate = json['requestDate'];
+    _reason = json['reason'];
+    _status = json['status'];
+    _totalLeaveDays = json['totalLeaveDays'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['leaveType'] = this._leaveType;
+    data['startDate'] = this._startDate;
+    data['endDate'] = this._endDate;
+    data['requestDate'] = this._requestDate;
+    data['reason'] = this._reason;
+    data['status'] = this._status;
+    data['totalLeaveDays'] = this._totalLeaveDays;
+    return data;
+  }
+}
+
